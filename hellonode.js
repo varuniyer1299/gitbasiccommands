44d3d961d1ad2407d42d89ffc945137066c918de
Iyer Varun <iyer.varun@publicissapient.com>
2021-03-26 09:43:02 +0530

    Modified hellonode

--- hellonode.js
+++ hellonode.js
@@ -1,2 +1,15 @@
 console.log("Hello node"); 
-  
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 5555;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
